8d10e903d91920527ce864e94a2917ae57b9ddd1
basl <basleenknegt@gmail.com>
2021-09-09 15:06:02 +0200

    Handle query parameters for annotation service

--- packages/annotation/asearch.py
+++ packages/annotation/asearch.py
@@ -4,7 +4,7 @@ that are connected to a segmented text object.
 '''
 
 def get_annotations_at_anchor(anchor, annotations, label=None):
-    annots_at_anchor = [ann_info for ann_info in annotations if ann_info['begin_anchor'] <= anchor <= ann_info['end_anchor']]  
+    annots_at_anchor = [ann_info for ann_info in annotations if ann_info['begin_anchor'] <= anchor <= ann_info['end_anchor']]
     return annots_at_anchor
 
 def get_annotations_of_type(type,annotations,resource_id=None):
@@ -18,24 +18,54 @@ def get_annotations_of_type(type,annotations,resource_id=None):
     if resource_id != None:
         annotations_for_resource = (a for a in annotations if a['resource_id'] == resource_id)
     return (a for a in annotations_for_resource if a['label'] == type)
-    
+
 def get_annotations_overlapping_with(begin_anchor,end_anchor,annotations,resource_id):
     '''
     Returns all annotations that overlap with a specific text interval.
     
     This function should work for all indexes or anchors that support comparison operations.
     '''
+    print('blurp')
     return (a for a in annotations if (a['resource_id'] == resource_id) and \
-    	   ((a['begin_anchor'] >= begin_anchor and a['begin_anchor'] < end_anchor) or\
-           (a['end_anchor'] > begin_anchor and a['end_anchor'] <= end_anchor) or\
-           (a['begin_anchor'] <= begin_anchor and a['end_anchor'] >= end_anchor)))
-           
+            ((a['begin_anchor'] >= begin_anchor and a['begin_anchor'] < end_anchor) or \
+             (a['end_anchor'] > begin_anchor and a['end_anchor'] <= end_anchor) or \
+             (a['begin_anchor'] <= begin_anchor and a['end_anchor'] >= end_anchor)))
+
+def matches_filters(annotation, filters):
+    if filters == None or len(filters) == 0:
+        return True
+
+    if 'type' in filters and annotation['label'] != filters['type']:
+        return False
+
+    if 'owner' not in filters:
+        return True
+
+    if 'owner' not in annotation or annotation['owner'] != filters['owner']:
+        return False
+
+    return True
+
+#    if annotation['label'] == filters['type']:
+#    	return True
+
+#    if annotation['owner'] == filters['owner']:
+#        return True
+#    else:
+#    	return False
+
+def get_filtered_annotations_overlapping(filters,begin,end,annotations,resource_id):
+    '''
+    Returns all annotations of that overlap with a specific text interval and match the filters.
+    '''
+    return filter(lambda annotation: matches_filters(annotation, filters), (get_annotations_overlapping_with(begin,end,annotations,resource_id)))
+
 def get_annotations_of_type_overlapping(type,begin,end,annotations,resource_id):
     '''
     Returns all annotations of a specific type that overlap with a specific text interval.
     '''
     return get_annotations_of_type(type,(get_annotations_overlapping_with(begin,end,annotations,resource_id)))
-    
+
 def get_annotations_of_types(types,annotations,resource_id=None):
     '''
     Returns all annotations of a list of given types.
@@ -44,12 +74,12 @@ def get_annotations_of_types(types,annotations,resource_id=None):
     if resource_id != None:
         annotations_for_resource = (a for a in annotations if a['resource_id'] == resource_id)
     return (a for a in annotations_for_resource if a['label'] in types)
-    
+
 def get_annotations_of_types_overlapping(types,begin,end,annotations,resource_id):
     '''
     Return all annotations of the given types that overlap with a specific text interval.
     '''
     return get_annotations_of_types(types,(get_annotations_overlapping_with(begin,end,annotations,resource_id)))
-    
+
 def get_annotation_by_id(id, annotations):
     return next(ann for ann in annotations if 'id' in ann.keys() and ann['id'] == id)
--- packages/annotation/aservice.py
+++ packages/annotation/aservice.py
@@ -17,14 +17,14 @@ app = Flask(__name__)
 
 app.json_encoder = segmentedtext.AnchorEncoder
 
-# datadir = '../../data/1728/10mrt-v1/'
-# annotation_repo = '1728-annotationstore.json'
-datadir = '../../data/output/'
-annotation_repo = 'tei_annotationstore.json'
+datadir = '../../data/1728/10mrt-v1/'
+annotation_repo = '1728-annotationstore.json'
+# datadir = '../../data/output/'
+# annotation_repo = 'tei_annotationstore.json'
 
 annotations = []
 anchors = {}
- 
+
 with open(datadir + annotation_repo, 'r') as filehandle:
     annotations = json.loads(filehandle.read())
 
@@ -36,37 +36,37 @@ def anchors_dict_2_obj(a):
     if isinstance(a['begin_anchor'], dict):
         ba_dict = a['begin_anchor']
         if anchors.get(ba_dict['identifier']) is None:
-            anchors[ba_dict['identifier']] = segmentedtext.Anchor(ba_dict['identifier'], ba_dict['sequence_number'])    
+            anchors[ba_dict['identifier']] = segmentedtext.Anchor(ba_dict['identifier'], ba_dict['sequence_number'])
         a['begin_anchor'] = anchors[ba_dict['identifier']]
 
         ea_dict = a['end_anchor']
         if anchors.get(ea_dict['identifier']) is None:
-            anchors[ea_dict['identifier']] = segmentedtext.Anchor(ea_dict['identifier'], ea_dict['sequence_number'])    
-        a['end_anchor'] = anchors[ea_dict['identifier']]   
-        
+            anchors[ea_dict['identifier']] = segmentedtext.Anchor(ea_dict['identifier'], ea_dict['sequence_number'])
+        a['end_anchor'] = anchors[ea_dict['identifier']]
+
 for a in annotations:
-    anchors_dict_2_obj(a)        
-       
+    anchors_dict_2_obj(a)
+
 @app.route('/', methods=['GET'])
 def identify_yourself():
     return jsonify({'message' : 'un-t-ann-gle Flask annotation service'})
-        
+
 @app.route('/annotations', methods=['GET', 'PUT'])
 def get_annotations():
     if request.method == 'PUT':
         global annotations
         # global anchors
         ann = request.get_json()
-        
+
         # substitute json dict anchor with Anchor instance
         anchors_dict_2_obj(ann)
-        
+
         annotations.append(ann)
         res = make_response(jsonify({"message": "Annotation appended"}), 200)
         return res
-        
+
     return jsonify({'annotations': annotations})
-    
+
 @app.route('/annotations/<int:index>', methods=['GET'])
 def get_annotation(index):
     return jsonify({'annotations': annotations[index]})
@@ -79,9 +79,9 @@ def returnAnnotationById(identifier):
         annotations.remove(ann)
         res = make_response(jsonify({"message": "Annotation deleted"}), 200)
         return res
-           
+
     return jsonify({'annotations': ann})
-    
+
 @app.route('/annotations/type/<string:type>', methods=['GET'])
 # REMARK: this is probably very inefficient because of copying of large lists
 def returnAnnotationsOfType(type):
@@ -93,19 +93,21 @@ def returnAnnotationsOfType(type):
 def returnAnnotationsOfTypeforResource(resource_id,type):
     annots = list(asearch.get_annotations_of_type(type,annotations,resource_id))
     return jsonify({'annotations' : annots})
-    
+
 @app.route('/<string:resource_id>/annotations/<int:begin_anchor>,<int:end_anchor>', methods=['GET'])
 def returnAnnotationsOverlappingWithInt(begin_anchor, end_anchor,resource_id):
-    annots = list(asearch.get_annotations_overlapping_with(begin_anchor,end_anchor,annotations,resource_id))
+    args = request.args
+    #    annots = list(asearch.get_annotations_of_type_overlapping(args['type'], begin_anchor,end_anchor,annotations,resource_id))
+    annots = list(asearch.get_filtered_annotations_overlapping(args, begin_anchor,end_anchor,annotations,resource_id))
     return jsonify({'annotations' : annots})
-        
+
 @app.route('/<string:resource_id>/annotations/<string:begin_anchor_id>,<string:end_anchor_id>', methods=['GET'])
 def returnAnnotationsOverlappingWithStr(begin_anchor_id, end_anchor_id,resource_id):
     begin_anchor = anchors[begin_anchor_id]
     end_anchor = anchors[end_anchor_id]
     annots = list(asearch.get_annotations_overlapping_with(begin_anchor,end_anchor,annotations,resource_id))
     return jsonify({'annotations' : annots})
-    
+
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
