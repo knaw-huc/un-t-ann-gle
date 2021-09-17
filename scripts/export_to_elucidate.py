cc854746f44f1414f2ef1897495f587271a32278
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2021-09-17 14:44:36 +0200

    small refactorings

--- scripts/export_to_elucidate.py
+++ scripts/export_to_elucidate.py
@@ -3,13 +3,11 @@
 import json
 import os
 
-import sys
-sys.path.append('../../elucidate-python-client/elucidate')
+import elucidate.tools as et
+from elucidate.client import ElucidateClient
 
-from client import split_annotation, ElucidateClient
-from icecream import ic
-
-from utils import default_progress_bar
+# elucidate_base_url = "https://elucidate.tt.di.huc.knaw.nl/annotation"
+elucidate_base_url = "http://localhost:18080/annotation"
 
 
 def main():
@@ -22,13 +20,13 @@ def main():
 
 
 def export_to_elucidate(web_annotations):
-    ec = ElucidateClient("https://elucidate.tt.di.huc.knaw.nl/annotation")
+    ec = ElucidateClient(elucidate_base_url)
     container_name = "republic_1728_06_19"
-    container_url = "https://elucidate.tt.di.huc.knaw.nl/annotation/w3c/republic-1728-06-19/"
-#    container_id = ec.read_container_identifier(container_name)
-#    if not container_id:
-#        container_id = ec.create_container(label=container_name, container_id=container_name)
-#    ic(container_id)
+    container_id = ec.read_container_identifier(container_name)
+    if not container_id:
+        container_id = ec.create_container(label=container_name, container_id=container_name)
+    #    ic(container_id)
+    # container_url = "https://elucidate.tt.di.huc.knaw.nl/annotation/w3c/republic-1728-06-19/"
 
     uploaded_json = "last_uploaded.json"
     if os.path.exists(uploaded_json):
@@ -38,12 +36,12 @@ def export_to_elucidate(web_annotations):
     else:
         print(f"- uploading {len(web_annotations)} annotations:")
         last_uploaded = -1
-#    bar = default_progress_bar(len(web_annotations) - last_uploaded)
+    #    bar = default_progress_bar(len(web_annotations) - last_uploaded)
     for i, wa in enumerate(web_annotations):
         if i > last_uploaded:
-#            bar.update(i - last_uploaded)
-            (body, target, custom) = split_annotation(wa)
-            ec.create_annotation(container_url=container_url, body=body, target=target, custom=custom)
+            #            bar.update(i - last_uploaded)
+            (body, target, custom) = et.split_annotation(wa)
+            ec.create_annotation(container_id=container_id, body=body, target=target, custom=custom)
             with open(uploaded_json, 'w') as f:
                 json.dump(i, f)
     print()
