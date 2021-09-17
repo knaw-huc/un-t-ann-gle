7ba93563a45773a5f097b855d45f9eb450d44091
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2021-09-17 14:44:57 +0200

    disable conversion examples

--- scripts/convert_to_web_annotations.py
+++ scripts/convert_to_web_annotations.py
@@ -125,7 +125,7 @@ def convert(input: str):
     scanpage_iiif_uri_map = {a['scan_id']: a['iiif_url'] for a in annotations if a['label'] == 'scanpage'}
     # ic(scanpage_iiif_uri_map)
 
-    print_example_conversions(annotations, scanpage_iiif_uri_map)
+    # print_example_conversions(annotations, scanpage_iiif_uri_map)
 
     web_annotations = convert_annotations(annotations, scanpage_iiif_uri_map)
 
