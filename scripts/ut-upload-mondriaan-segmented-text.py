16378c7affe9a9771ae9ffbeea35f983136b29d3
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2023-04-05 22:11:43 +0200

    add textrepo upload script

---
+++ scripts/ut-upload-mondriaan-segmented-text.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+from textrepo.client import TextRepoClient
+
+
+def main():
+    basedir = 'data'
+    tf_text_file = f"{basedir}/mondriaan-text.json"
+    trc = TextRepoClient("https://mondriaan.tt.di.huc.knaw.nl/textrepo", verbose=True)
+    external_id = "mondriaan-letters"
+    trc.purge_document(external_id)
+    doc_id = trc.create_document(external_id)
+    print(f"doc_id={doc_id}")
+    file_id = trc.create_document_file(doc_id, 1)
+    print(f"file_id={file_id}")
+    with open(tf_text_file) as f:
+        content = f.read()
+    version_id = trc.create_version(file_id=file_id.id, file=content)
+    print(f"version_id={version_id}")
+
+
+if __name__ == '__main__':
+    main()
