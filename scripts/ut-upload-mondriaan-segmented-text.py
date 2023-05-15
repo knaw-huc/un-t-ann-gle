1faa5edcc6d985e5c9ab7405b89fa3fe6beb472b
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2023-05-15 20:30:15 +0200

    use existing document/document_file

--- scripts/ut-upload-mondriaan-segmented-text.py
+++ scripts/ut-upload-mondriaan-segmented-text.py
@@ -6,15 +6,16 @@ def main():
     basedir = 'data'
     tf_text_file = f"{basedir}/mondriaan-text.json"
     trc = TextRepoClient("https://mondriaan.tt.di.huc.knaw.nl/textrepo", verbose=True)
-    external_id = "mondriaan-letters"
-    trc.purge_document(external_id)
-    doc_id = trc.create_document(external_id)
-    print(f"doc_id={doc_id}")
-    file_id = trc.create_document_file(doc_id, 1)
-    print(f"file_id={file_id}")
+    # external_id = "mondriaan-letters"
+    # trc.purge_document(external_id)
+    # doc_id = trc.create_document(external_id)
+    # print(f"doc_id={doc_id}")
+    # file_id = trc.create_document_file(doc_id, 1)
+    # print(f"file_id={file_id}")
+    file_id = "52086574-0c84-4ea0-b77b-ef11b6214252"
     with open(tf_text_file) as f:
         content = f.read()
-    version_id = trc.create_version(file_id=file_id.id, file=content)
+    version_id = trc.create_version(file_id=file_id, file=content)
     print(f"version_id={version_id}")
 
 
