feffbd0db23a9518fe3f4aaaab38dfd579e41b61
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2023-05-15 20:31:34 +0200

    set up end-to-end script

---
+++ scripts/tf-to-tr-ar.py
@@ -0,0 +1,16 @@
+version='0.8.4'
+url1=f"https://github.com/annotation/mondriaan/blob/master/watm/{version}/anno.json"
+url2=f"https://github.com/annotation/mondriaan/blob/master/watm/{version}/text.json"
+# download text.json
+# upload to tr; save version id
+# use version id when converting anno.json to web annotations
+# upload web-anno to ar
+# upload provenance:
+#   tf text.json url-> tr version url
+#   tf anno.json url -> ar container url
+
+# config.yml with:
+#     watm version
+#     tr url
+#     ar url
+#     container name
