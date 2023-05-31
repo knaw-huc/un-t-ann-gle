d25b6874a2e26f461181bbd8fe2f5c864c830551
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2023-05-31 23:07:32 +0200

    refactoring

--- scripts/caf-harvest.py
+++ scripts/caf-harvest.py
@@ -10,51 +10,57 @@ from loguru import logger
 
 
 # for each session in session output_dir, retrieve json data from proper CAF resolutions index
-def retrieve_res_json(query_string: str, date_string: str, caf_resolutions_output_dir: str):
-    logger.info(f"< {query_string}")
-    response = requests.get(query_string)
-    file_name = f'{date_string}-resolutions.json'
+def retrieve_res_json(query_base_url: str, date_string: str, caf_resolutions_output_dir: str):
+    # max_hits = requests.get(query_base_url + "&size=1").json()['hits']['total']['value']
+    max_hits = 10000
+    query_base_url = f"{query_base_url}&size={max_hits}"
+    logger.info(f"< {query_base_url}")
+    response = requests.get(query_base_url)
 
-    out_path = caf_resolutions_output_dir + file_name
+    file_name = f'{date_string}-resolutions.json'
+    out_path = f'{caf_resolutions_output_dir}/{file_name}'
+    size = len(response.json()['hits']['hits'])
+    if size == max_hits:
+        logger.warning("max_hits hits found! use search_after")
     logger.info(f"> {out_path}")
     with open(out_path, 'w') as filehandle:
         json.dump(response.json(), filehandle, indent=4)
 
 
 def harvest_year(year: str):
-    # year to harvest
     harvest_date = datetime.now().strftime("%y%m%d")
 
     # where to store harvest from CAF sessions index
-    # sessions_dump_file = f'sessions-{year}-output-19aug22.json'
-
     # output directories for session and resolution json_data
-    output_dir = f'./out/{harvest_date}/'
-    caf_sessions_outputdir = output_dir + f'CAF-sessions-{year}/'
-    caf_resolutions_outputdir = output_dir + f'CAF-resolutions-{year}/'
+    output_dir = f'./out/{harvest_date}'
+    caf_sessions_output_dir = f'{output_dir}/CAF-sessions-{year}'
+    caf_resolutions_output_dir = f'{output_dir}/CAF-resolutions-{year}'
+
     # pattern to filter session json files generated during the process
-    session_file_pattern = caf_sessions_outputdir + f"session-{year}-*"
+    session_file_pattern = f"{caf_sessions_output_dir}/session-{year}-*"
 
     # query strings
     session_query = f"https://annotation.republic-caf.diginfra.org/elasticsearch/session_lines/_doc/_search?" \
-                    f"q=metadata.session_year:{year}&size=10000"
+                    f"q=metadata.session_year:{year}"
     res_query_base = 'https://annotation.republic-caf.diginfra.org/elasticsearch/resolutions/_doc/_search?' \
-                     'size=1000&track_total_hits=true&q=metadata.session_id:'
+                     'track_total_hits=true&q=metadata.session_id:'
 
     # create output directories if they do not yet exist
     if not os.path.exists(output_dir):
         logger.info(f"creating {output_dir}")
         os.makedirs(output_dir)
 
-    if not os.path.exists(caf_sessions_outputdir):
-        logger.info(f"creating {caf_sessions_outputdir}")
-        os.makedirs(caf_sessions_outputdir)
+    if not os.path.exists(caf_sessions_output_dir):
+        logger.info(f"creating {caf_sessions_output_dir}")
+        os.makedirs(caf_sessions_output_dir)
 
-    if not os.path.exists(caf_resolutions_outputdir):
-        logger.info(f"creating {caf_resolutions_outputdir}")
-        os.makedirs(caf_resolutions_outputdir)
+    if not os.path.exists(caf_resolutions_output_dir):
+        logger.info(f"creating {caf_resolutions_output_dir}")
+        os.makedirs(caf_resolutions_output_dir)
 
     # start with harvesting all required session data from proper CAF session ES index
+    max_hits = requests.get(session_query + "&size=1").json()['hits']['total']['value']
+    session_query = f'{session_query}&size={max_hits}'
     logger.info(f"< {session_query}")
     response = requests.get(session_query)
 
@@ -64,15 +70,15 @@ def harvest_year(year: str):
     # generate separate session json file for each session in the ES response
     for session in response.json()['hits']['hits']:
         file_name = session['_id'] + '.json'
-        output_path = caf_sessions_outputdir + file_name
+        output_path = f"{caf_sessions_output_dir}/{file_name}"
         logger.info(f"> {output_path}")
         with open(output_path, 'w') as filehandle:
             json.dump(session, filehandle, indent=4)
     session_file_names = (f for f in glob.glob(session_file_pattern))
-    for n in session_file_names:
+    for n in sorted(session_file_names):
         base = os.path.basename(n)
         session_id = os.path.splitext(base)[0]
-        retrieve_res_json(f'{res_query_base}{session_id}', session_id, caf_resolutions_outputdir)
+        retrieve_res_json(f'{res_query_base}{session_id}', session_id, caf_resolutions_output_dir)
 
 
 def main():
