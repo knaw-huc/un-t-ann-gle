dcc8d39607fcfd3aa71ea223cb98f2e6f64e48e9
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2023-06-14 17:10:11 +0200

    remove temporary file

--- scripts/rp-extract-image-to-canvas.sh
+++ scripts/rp-extract-image-to-canvas.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 lines=$(grep http data/republic-volumes.csv | sed -e "s/.*http/http/g")
+tmpfile=data/image-to-canvas.csv.tmp
 
 for line in $lines; do
   uri=${line//[$'\t\r\n']} # remove line endings
   echo "<= ${uri}"
-  curl --silent $uri | jq  -r '.sequences[].canvases[] | [.label,."@id"] | @csv' >> data/image-to-canvas.csv.tmp
+  curl --silent $uri | jq  -r '.sequences[].canvases[] | [.label,."@id"] | @csv' >> $tmpfile
 done
-sort -u < data/image-to-canvas.csv.tmp > data/image-to-canvas.csv
+sort -u < $tmpfile > data/image-to-canvas.csv && rm $tmpfile
