9b0d7ad33c292a2136e8a6e119d0619b03e1ba2d
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2023-06-22 11:53:50 +0200

    use envfile

--- scripts/ut-run-republic-pipeline-for-year.sh
+++ scripts/ut-run-republic-pipeline-for-year.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 year=$1
+envfile=$2
 #harvestdir=out/230619
 harvestdir=out/$(date +"%y%m%d")
-
+source $envfile
 poetry run scripts/caf-harvest.py $year
 poetry run scripts/ut-untanngle-republic.py -d $harvestdir $year
-poetry run scripts/ut-upload-textstores.py -d $harvestdir -t https://textrepo.republic-caf.diginfra.org/api $year
+poetry run scripts/ut-upload-textstores.py -d $harvestdir -t https://textrepo.republic-caf.diginfra.org/api --provenance-base-url $PROV_URL -provenance-api-key $PROV_KEY $year
 version=$(jq -r ".[\"$year\"]" out/version_id_idx.json)
 poetry run scripts/rp-convert-to-web-annotations.py -t https://textrepo.republic-caf.diginfra.org/api/ -v $version -c data/image-to-canvas.csv -o $harvestdir/$year $harvestdir/$year/annotationstore-$year.json
-#poetry run scripts/upload-to-annorepo.py -a http://localhost:2023 -c republic-$year -k root $harvestdir/$year/web_annotations.json
-poetry run scripts/upload-to-annorepo.py -a https://annorepo.republic-caf.diginfra.org -c $year -l "Republic Year $year" $harvestdir/$year/web_annotations.json
+poetry run scripts/upload-to-annorepo.py -a http://localhost:2023 -c republic -k root $harvestdir/$year/web_annotations.json
+#poetry run scripts/upload-to-annorepo.py -a https://annorepo.republic-caf.diginfra.org -c $year -l "Republic Year $year" $harvestdir/$year/web_annotations.json
