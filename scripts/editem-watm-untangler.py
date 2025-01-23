7ae6e36339b80bbbcd6f38d2c5bdd5841b39756c
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2025-01-23 10:57:49 +0100

    editem-watm-untangler: get settings from yaml file

--- scripts/editem-watm-untangler.py
+++ scripts/editem-watm-untangler.py
@@ -1,32 +1,47 @@
 #!/usr/bin/env python3
-import os
 
 import untanngle.textfabric as tf
+import yaml
+from icecream import ic
+
+settings_path = '/editem/settings.yml'
 
 
 def main():
-    check_environment_variables()
+    settings = read_settings()
     config = tf.TFUntangleConfig(
-        project_name=os.environ['EDITEM_PROJECT'],
+        project_name=settings['project'],
         data_path='/watm',
         export_path=f'/out',
-        textrepo_base_uri=os.environ['EDITEM_TEXTREPO_URL'],
+        textrepo_base_uri=settings['textrepo'],
         excluded_types=[],
-        tier0_type=os.environ['EDITEM_TIER0_TYPE'],
-        with_facsimiles=os.environ['EDITEM_WITH_FACSIMILES']
+        tier0_type=settings['tier0'],
+        with_facsimiles=settings['with_facsimiles']
     )
     tf.untangle_tf_export(config)
 
 
-def check_environment_variables():
-    required = ['EDITEM_PROJECT', 'EDITEM_TEXTREPO_URL', 'EDITEM_TIER0_TYPE',
-                'EDITEM_WITH_FACSIMILES']
-    missing = [ev for ev in required if not ev in os.environ]
+required_settings = ['project', 'textrepo', 'tier0', 'with_facsimiles', 'annorepo']
+required_annorepo_settings = ['url', 'api_key', 'container']
+
+
+def read_settings():
+    with open(settings_path, "r") as f:
+        settings = yaml.safe_load(f)
+    if not settings:
+        settings = {}
+    missing = [s for s in required_settings if s not in settings]
+    if 'annorepo' in settings:
+        missing_annorepo = [f'annorepo.{s}' for s in required_annorepo_settings if s not in settings['annorepo']]
+        missing.extend(missing_annorepo)
+    else:
+        missing.extend([f'annorepo.{s}' for s in required_annorepo_settings])
     if missing:
-        print("missing environment variables; set these first")
-        for ev in missing:
-            print(f'  {ev}')
+        print(f"missing settings in {settings_path}:")
+        for s in missing:
+            print(f'  {s}')
         exit(-1)
+    return settings
 
 
 if __name__ == '__main__':
