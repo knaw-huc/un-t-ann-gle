4c3385267b7f75537a72bc617404a68981d10f17
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2025-05-20 14:39:38 +0200

    lambda to def

--- test/test_utils.py
+++ test/test_utils.py
@@ -1,4 +1,3 @@
-from pprint import pprint
 from unittest import TestCase
 
 from utils import transform_dict_entries
@@ -8,7 +7,12 @@ class Test(TestCase):
     def test_transform_dict_entries(self):
         dict_list = [{"a": 1}, {"a": 2}, {"a": 3}, {"b": {"a": -1}}]
         expected = [{"A": 2}, {"A": 3}, {"A": 4}, {"b": {"A": 0}}]
-        entry_filter = lambda k, v: k == "a"
-        entry_transformer = lambda k, v: (k.upper(), v + 1)
+
+        def entry_filter(k, v):
+            return k == "a"
+
+        def entry_transformer(k, v):
+            return k.upper(), v + 1
+
         transformed = transform_dict_entries(dict_list, entry_filter, entry_transformer)
         self.assertEqual(expected, transformed)
