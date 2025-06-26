397a1569ef5ca9c2154f6272a871246aaf769acf
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2025-06-26 19:11:38 +0200

    intro_text_factory: use plain numbers for the note n attribute

--- untanngle/intro_text_factory.py
+++ untanngle/intro_text_factory.py
@@ -23,10 +23,15 @@ class IntroTextFactory:
             "": "http://www.tei-c.org/ns/1.0"
         }
         self.xml_id_attrib = "{http://www.w3.org/XML/1998/namespace}id"
+        self.max_n = 0
+        self.note_num_offset = 0
+        self.section = 0
 
     def merge_intro_text_files(self) -> list[str]:
         elements = [[], [], [], []]
         for i, name in enumerate(self.intro_text_files):
+            self.max_n = 0
+            self.section = i + 1
             path = f"{self.input_xml_directory}/{name}.xml"
             if os.path.exists(path):
                 logger.info(f"<= {path}")
@@ -34,19 +39,22 @@ class IntroTextFactory:
                 root = tree.getroot()
 
                 elements[0].extend(
-                    [self._with_adjusted_ids(e, i + 1) for e in (root.find(".//div[@xml:lang='nl']", self.ns))]
+                    [self._with_adjusted_ids(e) for e in
+                     (root.find(".//div[@xml:lang='nl']", self.ns))]
                 )
                 elements[1].extend(
-                    [self._with_adjusted_ids(e, i + 1) for e in (root.find(".//div[@xml:lang='en']", self.ns))]
+                    [self._with_adjusted_ids(e) for e in
+                     (root.find(".//div[@xml:lang='en']", self.ns))]
                 )
 
                 notes_nl = root.find(".//listAnnotation[@xml:lang='nl']", self.ns)
                 if notes_nl:
-                    elements[2].extend([self._with_adjusted_ids(e, i + 1) for e in notes_nl])
+                    elements[2].extend([self._with_adjusted_ids(e) for e in notes_nl])
 
                 notes_en = root.find(".//listAnnotation[@xml:lang='en']", self.ns)
                 if notes_en:
-                    elements[3].extend([self._with_adjusted_ids(e, i + 1) for e in notes_en])
+                    elements[3].extend([self._with_adjusted_ids(e) for e in notes_en])
+                self.note_num_offset += self.max_n
             else:
                 self.errors.append(f"expected file {path} not found")
 
@@ -98,18 +106,20 @@ class IntroTextFactory:
             div.append(element)
         return div
 
-    def _with_adjusted_ids(self, e: ET.Element, i: int) -> ET.Element:
+    def _with_adjusted_ids(self, e: ET.Element) -> ET.Element:
+        i = self.section
         for ptr in e.findall(".//ptr", namespaces=self.ns):
             ptr.set("target", ptr.attrib["target"].replace("note", f"section.{i}.note"))
         if self.xml_id_attrib in e.attrib:
             e.set(self.xml_id_attrib, f"section.{i}." + e.attrib[self.xml_id_attrib])
             if "n" in e.attrib and "note" in e.attrib[self.xml_id_attrib]:
-                e.set("n", f"{i}-{e.attrib['n']}")
+                n = int(e.attrib["n"])
+                self.max_n = max(self.max_n, n)
+                e.set("n", f"{n + self.note_num_offset}")
         for element in e.findall(".//*[@xml:id]", namespaces=self.ns):
             element.set(self.xml_id_attrib, f"section.{i}." + element.attrib[self.xml_id_attrib])
         if "corresp" in e.attrib:
             e.set("corresp", e.attrib["corresp"].replace('#', f"#section.{i}."))
         for element in e.findall(".//*[@corresp]", namespaces=self.ns):
             element.set("corresp", element.attrib["corresp"].replace('#', f"#section.{i}."))
-
         return e
